0888fc65073fcbd7e696015751c4690cfd2ba46f
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-04 05:12:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  array.length do |value_to_find|
-    puts value_to_find
+  array[2] do |value_to_find|
+    return value_to_find
 end
 
 def find_max_value(array)
