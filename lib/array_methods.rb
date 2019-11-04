6c51967104b94397aa48cf5ef01622a5f8779b3e
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-04 05:38:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,5 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  
-  while array[counter] do
-    if array[counter] == value_to_find
-      return counter
-    end
-  counter += 1
-end
+ array.index(value_to_find)
 end
 
     
