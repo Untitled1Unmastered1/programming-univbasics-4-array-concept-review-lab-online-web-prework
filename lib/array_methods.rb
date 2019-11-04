c7e372142ad3d772f3d49e32edc988786b12ceea
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-04 05:57:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,8 +4,13 @@ end
 
 
 def find_max_value(array)
-  array.index 
-  puts 
+ counter = 5
+ 
+ while array[counter]do
+   if array[counter] == ma_x 
+     return counter
+   end
+   counter += 1 
 end
 
 def find_min_value(array)
